872ce318c0a12f1293a845314c12c536d02eba65
Greg Smith <gregsmith446@gmail.com>
2019-02-25 12:34:56 -0500

    initial commit empty slate

--- CapstoneConsoleApp/Program.cs
+++ CapstoneConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace CapstoneConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Data!");
         }
     }
 }
---
+++ README.md.txt
@@ -0,0 +1,1 @@
+This project uses Selenium Web Driver to scrape Stock Data to the Console from a Yahoo Finance Portfolio.
